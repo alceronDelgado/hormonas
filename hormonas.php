09f257f44aa33c0357b16063a0a0feecc02855da
alceronDelgado <luis_aceron@soy.sena.edu.co>
2024-10-18 23:07:44 -0500

    agregado lógica y parte visual

---
+++ hormonas.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+  $nombrePaciente = $_POST['nombrePaciente'];
+  $identificador = $_POST['identificador'];
+  $edad = $_POST['edad'];
+  $hormonas = $_POST['hormonas'];
+  $resultado = "";
+
+  if  (
+      (($edad >= 12) && ($edad <= 14) && ($nivelHormonas < 14) && ($nivelHormonas < 27)) ||
+      (($edad >= 15) && ($edad <= 16) && ($nivelHormonas < 11) && ($nivelHormonas < 19)) ||
+      (($edad >= 17) && ($edad <= 20) && ($nivelHormonas < 11) && ($nivelHormonas < 15)) ||
+      (($edad >= 21) && ($edad <= 24) && ($nivelHormonas < 11.5) && ($nivelHormonas < 15)) ||
+      (($edad >= 25) && ($edad <= 33) && ($nivelHormonas < 13.6) && ($nivelHormonas < 16.5))
+  ) {
+      $resultado = "Estas embarazada";
+
+  }else{
+      $resultado = "No Estas embarazada";
+  }
+
+  //creamos array y le guardamos unos valores
+  $items = array($nombrePaciente,$identificador,$edad,$nivelHormonas,$resultado);
+
+  if (empty($_SESSION['respuesta'])) {
+    //creamos variable de session y le decimos que va a tener el array
+    $_SESSION['respuesta'] = $items;
+  }
+
+  //Redireccionamos al index de la página con la respuesta
+  header("location: http://localhost:8080/hormonas/");
+?>
--- index.php
+++ index.php
@@ -1,21 +1,66 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+
   <link rel="stylesheet" href="assets/css/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hormonas</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-  <div class="header">
-    <h1>Hormonas</h1>
-  </div>
+  <header class="header">
+    <div class="logo">
+      <img src="assets/img/viva1a.png" alt="" id="logotipo" class="" width="100%" height="80px">
+    </div>
+    <div class="title">
+      <h1 id="textTitle">IPS VIVA1A</h1>
+    </div>
+  </header>
 
   <div class="container">
-    <form action="resultado.php" method="post">
-      <input type="text" name="" id="">
-      <button>hello</button>
-    </form>
+    <div class="items">
+      <div class="img">
+        <img src="assets/img/pregnant.png" alt="" id="pregnant">
+      </div>
+      <div class="form ">
+        <form action="hormonas.php" method="post" class="form1">
+          <div class="grupo1">
+            <input type="text" name="nombrePaciente" id="" placeholder="Nombre Paciente: " class="item form-control">
+            <select name="identificador" id="" class="item form-select">
+              <option value="---" selected>--</option>
+              <option value="CC">CC</option>
+              <option value="TI">TI</option>
+            </select>
+          </div>
+          <div class="grupo2">
+            <input type="number" name="edad" id="" placeholder="Edad paciente: " min="0" max="100" class="item form-control">
+            <input type="number" name="hormonas" id="" placeholder="Nivel Hormonas: " min="0" max="100" class="item form-control">
+          </div>
+          <div class="grupo3">
+            <button id="btn" class="btn btn-outline-info">hello</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+  </div>
+
+  <div class="result">
+    <?php
+    if (isset($_SESSION['respuesta'])) {
+      for ($i=0; $i < count($_SESSION['respuesta']); $i++) {
+        echo $_SESSION['respuesta'][$i]."\n";
+      }
+      session_destroy();
+    }else{
+      echo "";
+    }
+    ?>
   </div>
 </body>
 </html>
